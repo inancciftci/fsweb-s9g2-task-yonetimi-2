f360103047c4c6c8a4d7cf740b4d14a101046171
3nonch <enonch@hotmail.com>
2023-12-17 09:31:17 +0300

    proje tamamlandi

--- src/App.js
+++ src/App.js
@@ -4,24 +4,23 @@ import Task from "./Task";
 import TaskHookForm from "./TaskHookForm";
 import PeopleForm from "./PeopleForm";
 import { initialTasks, initialTeam } from "./data";
-import { toast } from 'react-toastify';
-
+import { toast } from "react-toastify";
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
-    setTasks([yeniTask, ...tasks])
+    setTasks([yeniTask, ...tasks]);
   }
 
   function handlePeopleSubmit(yeniKisi) {
-    setTeam([...team, yeniKisi])
+    setTeam([...team, yeniKisi]);
   }
 
   function handleComplete(id) {
     const tasksCopy = [...tasks];
-    const ilgiliTask = tasksCopy.filter(t => t.id === id)[0];
+    const ilgiliTask = tasksCopy.filter((t) => t.id === id)[0];
     ilgiliTask.status = "yapıldı";
     setTasks(tasksCopy);
 
@@ -29,21 +28,21 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <div className="formColumn">
-        <div className="form-container">
-          <h2>Yeni Task</h2>
+    <div className="h-screen flex">
+      <div className="bg-[#fff] basis-96 border-r-2 border-solid border-[#f3d4b0] overflow-auto">
+        <div className="pt-4 px-2 pb-6 border-b-8 border-solid border-[#ddd]">
+          <h2 className="text-2 pb-2  ">Yeni Task</h2>
           <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
         </div>
 
-        <div className="form-container">
+        <div className="pt-4 px-2 pb-6 border-b-8 border-solid border-[#ddd]">
           <h2>Yeni Kişi</h2>
           <PeopleForm kisiler={team} submitFn={handlePeopleSubmit} />
         </div>
       </div>
-      <div className="columns">
-        <div className="column">
-          <h2 className="column-title">Yapılacaklar</h2>
+      <div className="flex-1 flex justify-center flex-wrap pt-8 px-8 pb-6 gap-8 overflow-auto">
+        <div className="flex-1 min-w-[240px] max-w-[360px] ">
+          <h2 className="text-2xl pl-3 ">Yapılacaklar</h2>
           <div className="column-list">
             {tasks
               .filter((t) => t.status === "yapılacak")
@@ -63,7 +62,6 @@ function App() {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
